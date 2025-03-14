8c2629a7180f9da772cccf26b0f213cc7d84f887
123jason689 <jason.melvin689@gmail.com>
2025-03-14 13:43:00 +0700

    add a main landing page

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
+import dotenv from "dotenv"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,20 @@ export default defineConfig({
     tailwindcss()
     
   ],
+  define:{
+    "process.env.VITE_APIKEY": JSON.stringify(process.env.VITE_APIKEY),
+    "process.env.VITE_AUTHDOMAIN": JSON.stringify(process.env.VITE_AUTHDOMAIN),
+    "process.env.VITE_DATABASEURL": JSON.stringify(process.env.VITE_DATABASEURL),
+    "process.env.VITE_PROJECTID": JSON.stringify(process.env.VITE_PROJECTID),
+    "process.env.VITE_STORAGEBUCKET": JSON.stringify(process.env.VITE_STORAGEBUCKET),
+    "process.env.VITE_MESSAGINGSENDERID": JSON.stringify(process.env.VITE_MESSAGINGSENDERID),
+    "process.env.VITE_APPID": JSON.stringify(process.env.VITE_APPID),
+    "process.env.VITE_MEASUREMENTID": JSON.stringify(process.env.VITE_MEASUREMENTID)
+  },
+  server: {
+    watch: {
+      usePolling: true, // Enable polling to detect changes
+    },
+    hmr: true, // Enable Hot Module Replacement
+  },
 })
